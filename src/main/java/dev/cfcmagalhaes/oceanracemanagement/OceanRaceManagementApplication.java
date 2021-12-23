8a0ca260a990c5ba66b706a45ac080217dba42a8
cfcmagalhaes <cfcmagalhaes@yahoo.com.br>
2021-12-22 21:01:15 -0300

    Commit inicial

---
+++ src/main/java/dev/cfcmagalhaes/oceanracemanagement/OceanRaceManagementApplication.java
@@ -0,0 +1,13 @@
+package dev.cfcmagalhaes.oceanracemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceanRaceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OceanRaceManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cfcmagalhaes/oceanracemanagement/OceanRaceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cfcmagalhaes.oceanracemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OceanRaceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
